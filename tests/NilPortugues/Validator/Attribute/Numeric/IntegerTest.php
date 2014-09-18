bd7f03717d3a2126a9a9c98445ca71a5efd2d9a2
Nil Portugués Calderó <nilopc@gmail.com>
2014-09-18 21:00:11 +0200

    Working on code coverage

---
+++ tests/NilPortugues/Validator/Attribute/Numeric/IntegerTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 9/18/14
+ * Time: 8:47 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\NilPortugues\Validator\Attribute\Numeric;
+
+
+class IntegerTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
