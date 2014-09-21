982afcccc69d0485d0c84598c84d15f513218dc4
Nil Portugués Calderó <nilopc@gmail.com>
2014-09-21 21:01:58 +0200

    Working on DateTime Validator.

---
+++ src/NilPortugues/Validator/Attribute/DateTime/DateTime.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Nil Portugués Calderó <contact@nilportugues.com>
+ * Date: 9/21/14
+ * Time: 8:37 PM
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NilPortugues\Validator\Attribute\DateTime;
+
+
+/**
+ * Class DateTime
+ * @package NilPortugues\Validator\Attribute\DateTime
+ */
+class DateTime {
+
+} 
