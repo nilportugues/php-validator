1ca2839253858bbf8bbab73b9dfb68ffbe831b86
Nil Portugués <nil.portugues@atrapalo.com>
2014-09-30 17:59:13 +0200

    WIP validation example for all form inputs

---
+++ examples/all_input_types.php
@@ -0,0 +1,33 @@
+<form>
+
+    <h4>Gender</h4>
+    <input type="radio" name="input_radio" value="male">Male<br>
+    <input type="radio" name="input_radio" value="female">Female
+
+    <h4>Pick at least one</h4>
+    <input type="checkbox" name="input_checkbox" value="Bike">I have a bike<br>
+    <input type="checkbox" name="input_checkbox" value="Car">I have a car 
+
+    <h4>Select One</h4>
+    <select name="input_select_one">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="fiat" selected>Fiat</option>
+    <option value="audi">Audi</option>
+    </select>
+
+    <h4>Select Many</h4>
+    <select name="input_many">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="fiat" selected>Fiat</option>
+    <option value="audi">Audi</option>
+    </select>
+
+    <h4>A text field</h4> 
+    <input type="text" name="input_text">
+
+    <h4>A text area field</h4> 
+    <textarea name="input_textarea"rows="10" cols="30"></textarea>
+
+</form>
