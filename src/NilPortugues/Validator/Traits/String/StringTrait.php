ea6791c765cbb32321d41bd9e31b5f3f077c0b33
Nil Portugués Calderó <nilopc@gmail.com>
2014-11-27 16:25:22 +0100

    Update StringTrait.php

--- src/NilPortugues/Validator/Traits/String/StringTrait.php
+++ src/NilPortugues/Validator/Traits/String/StringTrait.php
@@ -484,7 +484,7 @@ trait StringTrait
         settype($value, 'string');
 
         $pattern = '/^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$/i';
-        if (true != $strict) {
+        if (true !== $strict) {
             $value   = trim($value, '[]{}');
             $pattern = '/^[a-f0-9]{4}(?:-?[a-f0-9]{4}){7}$/i';
         }
